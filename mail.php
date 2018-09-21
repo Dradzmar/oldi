aac4d96b8d78f6b3d173aebef83933b7e0fcda09
Alexandr Kharchenko <glad1uss967@gmail.com>
2018-09-21 11:39:19 +0300

    update

--- mail.php
+++ mail.php
@@ -1,12 +1,22 @@
 <?php
-$to = "help@oldi.io";
+$to = "alex.h@praid.com.ua";
 $tema = "Форма обратной связи oldi";
-$message = "Ваше имя: ".$_POST['name']."<br>";
+$message = "Имя: ".$_POST['name']."<br>";
 $message .= "E-mail: ".$_POST['email']."<br>";
 $message .= "Номер телефона: ".$_POST['phone']."<br>";
 $message .= "Сообщение: ".$_POST['message']."<br>";
 $headers = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-$headers .= 'From: test@oldi.io' . "\r\n";
-mail($to, $tema, $message, $headers);
+$headers .= 'From: tratata@tratata.tata' . "\r\n";
+$send = mail($to, $tema, $message, $headers);
+if ($send == 'true')
+ 
+{
+echo "<b>Спасибо за отправку вашего сообщения!<p>";
+echo "<a href=index.html>Нажмите,</a> чтобы вернуться на главную страницу";
+}
+else 
+{
+echo "<p><b>Ошибка. Сообщение не отправлено!";
+}
 ?>
--- sub.php
+++ sub.php
@@ -1,9 +1,9 @@
 <?php
-$to = "help@oldi.io";
+$to = "alex.h@praid.com.ua";
 $tema = "Форма подписки";
 $message .= "E-mail: ".$_POST['email']."<br>";
 $headers = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-$headers .= 'From: test@oldi.io' . "\r\n";
+$headers .= 'From: tratata@tratata.tata' . "\r\n";
 mail($to, $tema, $message, $headers);
 ?>
